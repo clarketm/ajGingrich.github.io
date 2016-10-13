f476c8b1b46275e9fcf72b6308bcb295b3090186
Andrew Gingrich <ajgingrich@yahoo.com>
2016-10-13 17:42:15 -0600

    Initialize project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Andrew on 13/10/2016.
+ */
+var newImage = function () {
+    document.getElementById("indexImage").src = "img/skulls.jpg";
+}
+var oldImage = function () {
+    document.getElementById("indexImage").src = "img/mountains.jpg";
+}
