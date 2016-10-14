2bdb412ceeb8a120bb5ec6912c39c8413582e05d
Andrew Gingrich <ajgingrich@yahoo.com>
2016-10-14 13:11:03 -0600

    add san nicolas link

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 /**
  * Created by Andrew on 13/10/2016.
  */
-var newImage = function () {
+/*var newImage = function () {
     document.getElementById("indexImage").src = "img/skulls.jpg";
 }
 var oldImage = function () {
     document.getElementById("indexImage").src = "img/mountains.jpg";
-}
+}*/
+$(document).ready(function() {
+    $('#hello').click(function() {
+        $(this).effect("bounce", {times:10}, 3000);
+    });
+});
