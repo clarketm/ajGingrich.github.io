5bf6d2373afc4b57d4c43037764bca45385c6605
Travis Clarke <Travis.Clarke@teamaol.com>
2016-10-15 22:41:57 -0700

    js cleanup

--- js/script.js
+++ js/script.js
@@ -2,36 +2,37 @@
  * Created by Andrew on 13/10/2016.
  */
 
-$(document).ready(function() {
+/*	Slideshow	*/
+var $clips = $(".clip"),
+    clipIndex = 0,
+    numClips = $clips.length,
+    sliderSpeed = 5000,
+    sliderEffect = 1000,
+
+    fadeOutCurrent = function() {
+        $($clips[clipIndex++ % numClips]).fadeOut(sliderEffect, function() {
+            $(this).removeClass("currentClip");
+        });
+    },
+
+    fadeInNext = function () {
+        $($clips[clipIndex % numClips]).fadeIn(sliderEffect, function () {
+            $(this).addClass("currentClip");
+        });
+    },
+
+    initSlider = function () {
+        setInterval(function() {
+            fadeOutCurrent();
+            fadeInNext();
+        }, sliderSpeed);
+    };
+
+
+/* DOM ready*/
+$(function(){
     $('#hello').click(function() {
         $(this).effect("bounce", {distance: 40, times:10}, 3000);
     });
+    initSlider()
 });
-
-/*	Slideshow	*/
-
-var numClips = $("#sliderMain .clip").length;
-var clipCount = 0;
-var sliderSpeed = 5000;
-var sliderEffect = 1000;
-
-$(document).ready(function(){
-    setInterval(function() {
-        $("#sliderMain .currentClip").fadeOut(sliderEffect).removeClass("currentClip");
-        $("#sliderMain .clip:eq("+clipCount+")").fadeIn(sliderEffect).addClass("currentClip");
-        clipCount++;
-        if(clipCount == numClips){
-            clipCount = 0;
-        }
-    },
-        sliderSpeed);
-});
-
-
-
-
-
-
-
-
-
