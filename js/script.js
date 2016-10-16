dac88afbf7c7fa5f34e9769a904498e08921575f
Andrew Gingrich <ajgingrich@yahoo.com>
2016-10-15 21:57:33 -0600

    add slideshow

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
 /**
  * Created by Andrew on 13/10/2016.
  */
-/*var newImage = function () {
-    document.getElementById("indexImage").src = "img/skulls.jpg";
-}
-var oldImage = function () {
-    document.getElementById("indexImage").src = "img/mountains.jpg";
-}*/
+
 $(document).ready(function() {
     $('#hello').click(function() {
         $(this).effect("bounce", {distance: 40, times:10}, 3000);
     });
 });
+
+/*	Slideshow	*/
+
+var numClips = $("#sliderMain .clip").length;
+var clipCount = 0;
+var sliderSpeed = 5000;
+var sliderEffect = 1000;
+
+$(document).ready(function(){
+    setInterval(function() {
+        $("#sliderMain .currentClip").fadeOut(sliderEffect).removeClass("currentClip");
+        $("#sliderMain .clip:eq("+clipCount+")").fadeIn(sliderEffect).addClass("currentClip");
+        clipCount++;
+        if(clipCount == numClips){
+            clipCount = 0;
+        }
+    },
+        sliderSpeed);
+});
+
+
+
+
+
+
+
+
+
